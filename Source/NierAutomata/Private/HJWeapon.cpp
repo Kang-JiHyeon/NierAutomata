3d9f6e6eb5abf6cadee3d50de96d647d947bb492
Spacialist <a01050254473@gmail.com>
2024-07-05 00:04:56 +0900

    0705_0005_Gameover UI
    엔딩 UI 추가

--- Source/NierAutomata/Private/HJWeapon.cpp
+++ Source/NierAutomata/Private/HJWeapon.cpp
@@ -8,9 +8,15 @@ AHJWeapon::AHJWeapon()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	// 콜리전 박스 설정
+	CompBox = CreateDefaultSubobject<UBoxComponent>(TEXT("BOX"));
+	RootComponent = CompBox;
+	CompBox->SetBoxExtent(FVector(8.f, 16.f, 100.f));
+
+
 	// 스케레탈 메시 객체 부여 
 	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("WEAPON"));
-	RootComponent = Weapon;
+	Weapon->SetupAttachment(CompBox);
 
 	ConstructorHelpers::FObjectFinder<USkeletalMesh>
 		tempWeapon(TEXT("/Script/Engine.SkeletalMesh'/Game/Mesh/SK_Blade_BlackKnight.SK_Blade_BlackKnight'"));
