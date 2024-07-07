9eb08613b0601053b725bdcaf24137b769b09c77
Kang Jihyeon <65221707+Kang-JiHyeon@users.noreply.github.com>
2024-07-07 23:22:21 +0900

    SpiralMove 기능 테스트

---
+++ Source/NierAutomata/Private/JHSpiralMoveSkill.cpp
@@ -0,0 +1,101 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "JHSpiralMoveSkill.h"
+
+// Sets default values for this component's properties
+UJHSpiralMoveSkill::UJHSpiralMoveSkill()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+// Called when the game starts
+void UJHSpiralMoveSkill::BeginPlay()
+{
+	Super::BeginPlay();
+
+	MyOwner = GetOwner();
+
+	CenterPos = MyOwner->GetActorLocation();
+}
+
+
+// Called every frame
+void UJHSpiralMoveSkill::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// 일정 시간동안 확장하는 방향으로 나선형으로 이동하고 싶다.
+	// 일정 시간이 지나면 다시 축소하는 방향으로 나선형 이동하고 싶다.
+	// - 일정 시간
+	// - 방향
+	// - 이동 대상
+	// - 원점 좌표
+	// 각도
+
+	if (bIsAttack)
+	{
+		OnAttack();
+	}
+	else
+	{
+		OnInitialize();
+	}
+
+}
+
+void UJHSpiralMoveSkill::OnInitialize()
+{
+	CurrTime = 0;
+	Sign = 1;
+	Radius = 0;
+	DegreeAngle = 0;
+	CenterPos = FVector(0, 0, 0);
+}
+
+void UJHSpiralMoveSkill::OnAttack()
+{
+	if (MyOwner == nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Owner가 Null입니다."));
+		return;
+	}
+
+	//if (CurrTime < 0)
+	//	Sign = 1;
+	//else if (CurrTime > MoveTime)
+	//	Sign = -1;
+	if (CurrTime < 0)
+		return;
+
+	if (CurrTime > MoveTime)
+		Sign = -1;
+
+
+	float DeltaTime = GetWorld()->DeltaTimeSeconds;
+
+	CurrTime += Sign * DeltaTime;
+
+	Radius += Sign * RadiusSpeed * DeltaTime;
+	DegreeAngle += AngleSpeed * DeltaTime;
+
+	if (DegreeAngle > 360)
+		DegreeAngle = 0;
+
+	float RadianAngle = FMath::DegreesToRadians(DegreeAngle);
+
+	float X = CenterPos.X + FMath::Cos(RadianAngle) * Radius;
+	float Y = CenterPos.Y + FMath::Sin(RadianAngle) * Radius;
+
+	MyOwner->SetActorLocation(CenterPos + FVector(X, Y, 0));
+}
+
+void UJHSpiralMoveSkill::SetCenterPosition(FVector Position)
+{
+	CenterPos = Position;
+}
+
---
+++ Source/NierAutomata/Public/JHSpiralMoveSkill.h
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "JHAttackInterface.h"
+#include "JHSpiralMoveSkill.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class NIERAUTOMATA_API UJHSpiralMoveSkill : public UActorComponent, public IJHAttackInterface
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UJHSpiralMoveSkill();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	virtual void OnInitialize() override;
+
+	virtual void OnAttack() override;
+
+private:
+
+	// 각도
+	UPROPERTY(VisibleAnywhere)
+	float Radius;
+
+	UPROPERTY(EditAnywhere)
+	float RadiusSpeed = 300;
+
+	UPROPERTY(VisibleAnywhere)
+	float DegreeAngle;
+
+	UPROPERTY(EditAnywhere)
+	float AngleSpeed = 100;
+
+	// 시간
+	UPROPERTY(EditAnywhere)
+	float MoveTime = 5;
+
+	UPROPERTY(VisibleAnywhere)
+	float CurrTime;
+
+	UPROPERTY(VisibleAnywhere)
+	float Sign = 1;
+
+	// Test
+	UPROPERTY(EditAnywhere)
+	bool bIsAttack;
+
+	AActor* MyOwner;
+	FVector CenterPos;
+
+
+public:
+	void SetCenterPosition(FVector Position);
+
+};
